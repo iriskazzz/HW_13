7ab2d99551f7ab31654428b25656ebe7f48f568b
Irina Zolina <JaredLeto@@2317>
2023-10-17 15:59:33 +0300

    added DesiredCapabilities

--- src/test/java/com/demoqa/tests/RegistrationFormPageObjectsTest.java
+++ src/test/java/com/demoqa/tests/RegistrationFormPageObjectsTest.java
@@ -1,8 +1,13 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.page.RegistrationFormPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 
 public class RegistrationFormPageObjectsTest {
@@ -12,7 +17,25 @@ public class RegistrationFormPageObjectsTest {
   static void configure() {
     Configuration.pageLoadStrategy = "eager";
     Configuration.baseUrl = "https://demoqa.com";
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+            "enableVNC", true,
+            "enableVideo", true
+    ));
+    Configuration.browserCapabilities = capabilities;
   }
+
+//  @AfterEach
+//  void addAttachments() {
+//    Attach.screenshotAs("Last screenshot");
+//    Attach.pageSource();
+//    Attach.browserConsoleLogs();
+//    Attach.addVideo();
+//  }
 
   @Tag("regress")
   @Test
