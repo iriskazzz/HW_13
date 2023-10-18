f4c262933a2f2a6f11d3dc51a15d8acb6c083ab6
Irina Zolina <JaredLeto@@2317>
2023-10-18 11:33:34 +0300

    added @Step

--- src/test/java/com/demoqa/page/RegistrationFormPage.java
+++ src/test/java/com/demoqa/page/RegistrationFormPage.java
@@ -3,6 +3,7 @@ package com.demoqa.page;
 import com.codeborne.selenide.SelenideElement;
 import com.demoqa.page.components.CalendarComponent;
 import com.demoqa.page.components.ResultModal;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -27,6 +28,7 @@ public class RegistrationFormPage {
           cityInput = $("#stateCity-wrapper"),
           submitButton = $("#submit");
 
+  @Step("Открытие главное страницы")
   public RegistrationFormPage openPage() {
     open("/automation-practice-form");
     executeJavaScript("$('footer').remove()");
@@ -48,36 +50,42 @@ public class RegistrationFormPage {
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Имя")
   public RegistrationFormPage setFirstName(String value) {
     firstNameInput.setValue(value);
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Фамилия")
   public RegistrationFormPage setLastName(String value) {
     lastNameInput.setValue(value);
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Почта")
   public RegistrationFormPage setEmail(String value) {
     emailInput.setValue(value);
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Номер")
   public RegistrationFormPage setNumber(String value) {
     numberInput.setValue(value);
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Пол")
   public RegistrationFormPage setGenter(String value) {
     genderInput.$(byText(value)).click();
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Дата рождения")
   public RegistrationFormPage setBirthDate(String day, String month, String year) {
     $("#dateOfBirthInput").click();
     calendarComponent.setDate(day, month, year);
@@ -85,30 +93,35 @@ public class RegistrationFormPage {
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Тема")
   public RegistrationFormPage setSubjects(String value) {
     subjectInput.setValue(value).pressEnter();
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Хобби")
   public RegistrationFormPage setHobbies(String value) {
     hobbiesInput.$(byText(value)).click();
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Фото")
   public RegistrationFormPage setPicture(String value) {
     pictureInput.uploadFromClasspath(value);
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Адрес")
   public RegistrationFormPage setCurrentAddress(String value) {
     currentAddressInput.setValue(value);
 
     return this;
   }
 
+  @Step("Ввод значения {value} в поле Штат")
   public RegistrationFormPage setState(String value) {
     $("#state").click();
     stateInput.$(byText(value)).click();
@@ -116,6 +129,7 @@ public class RegistrationFormPage {
     return this;
   }
 
+  @Step("Ввод значения {value} в поле город")
   public RegistrationFormPage setCity(String value) {
     $("#city").click();
     cityInput.$(byText(value)).click();
@@ -123,18 +137,21 @@ public class RegistrationFormPage {
     return this;
   }
 
+  @Step("Проверка, что модальное окно с результатами открыто")
   public RegistrationFormPage checkResultTable() {
     resultModal.checkVisible();
 
     return this;
   }
 
+  @Step("Проверка соответствия поля {key} и его значения {value}")
   public RegistrationFormPage checkResult(String key, String value) {
     resultModal.checkResults(key, value);
 
     return this;
   }
 
+  @Step("Клик по кнопке Submit")
   public RegistrationFormPage clickSubmit() {
     submitButton.click();
 
